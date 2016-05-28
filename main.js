0cde23653e4b2e0649fffeb27e8753b11fb0bf1a
Stoh <sim.nowak@gmail.com>
2016-05-29 00:42:01 +0200

    log du time a chaque frame

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 var LegacyExtends = require('LegacyExtends');
 var Rooms = require('Rooms');
 
-var rooms = new Rooms();
-
 module.exports.loop = function () {
-    
-    // console.log('energy: ' + Game.rooms.sim.energyAvailable);
-    console.log("cpu used: " + Game.cpu.getUsed());
 
-    rooms.run();
-    
+    // console.log('energy: ' + Game.rooms.sim.energyAvailable);
+    console.log(Game.time "> cpu used: " + Game.cpu.getUsed());
+
+    Rooms.instance().run();
 }
