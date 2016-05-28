3653291e0b086d5cb7b0f77a326b16d7e138f74c
Stoh <sim.nowak@gmail.com>
2016-05-28 19:16:10 +0200

    no energy transfer to spawner

--- LegacyExtends.js
+++ LegacyExtends.js
@@ -3,7 +3,10 @@ var RoleEnergy = require('RoleEnergy');
 var LegacyExtends = {};
 
 Creep.prototype.needsEnergyTransfer = function() {
-    return this.memory.role != 'energy' && this.memory.role != 'harvester' && this.carry.energy < this.carryCapacity;
+    return this.memory.role != 'energy' &&
+        this.memory.role != 'harvester' &&
+        this.memory.role != 'spawner' &&
+        this.carry.energy < this.carryCapacity;
 }
 
 Structure.prototype.needsEnergyTransfer = function() {
