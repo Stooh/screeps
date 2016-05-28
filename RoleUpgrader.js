1e8512a6f5b03dcbf50f538230c42b8f8a7ba111
Stoh <sim.nowak@gmail.com>
2016-05-28 19:21:22 +0200

    Upgrader go to controler before having energy

--- RoleUpgrader.js
+++ RoleUpgrader.js
@@ -2,15 +2,14 @@ var RoleUpgrader = {};
 
 /** @param Creep creep */
 RoleUpgrader.run = function(creep, roomCache) {
+    var controller = creep.room.controller;
     if(creep.carry.energy == 0) {
-        /*var sources = creep.room.find(FIND_SOURCES);
-        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[0]);
-        }*/
+        if(!creep.pos.isNearTo(controller))
+            creep.moveTo(creep.room.controller);
     }
     else {
-        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(creep.room.controller);
+        if(creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(controller);
         }
     }
 };
