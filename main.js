bfeec6a808f2b02f0b3b3d428deca861140790c7
Stoh <sim.nowak@gmail.com>
2016-05-29 04:19:46 +0200

    syntax error

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var Rooms = require('Rooms');
 module.exports.loop = function () {
 
     // console.log('energy: ' + Game.rooms.sim.energyAvailable);
-    console.log(Game.time "> cpu used: " + Game.cpu.getUsed());
+    console.log(Game.time + "> cpu used: " + Game.cpu.getUsed());
 
     Rooms.instance().run();
 }
