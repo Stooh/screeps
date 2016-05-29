8412991e0a2a671c2018827086895ce97ad5fca4
Stoh <sim.nowak@gmail.com>
2016-05-29 18:33:29 +0200

    less dumb target choice for builder

--- RoleBuilder.js
+++ RoleBuilder.js
@@ -1,13 +1,39 @@
 var RoleBuilder = {};
 
+function getTarget(creep) {
+    var tId = creep.memory.target;
+    return tId ? Game.getObjectById(tId) : undefined;
+}
+
+function choseTargetSite(creep, roomHandler) {
+    // choose a new target
+    var targets = roomHandler.cache.constructionSitesSafe();
+    if(!targets.length)
+        return;
+
+    var target = creep.pos.findClosestByRange(targets);
+    if(!target)
+        return;
+
+    creep.memory.target = target.id;
+    return target;
+}
+
 /** @param Creep creep */
 RoleBuilder.run = function(creep, roomHandler) {
-    var targets = roomHandler.cache.constructionSites();
-    if(targets.length) {
-        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets[0]);
-        }
+    var target = getTarget(creep);
+    if(!target) {
+        target = choseTargetSite(creep, roomHandler);
+
+        if(!target)
+            return;
     }
+
+    var err = creep.build(target)
+    if(err == ERR_NOT_IN_RANGE)
+        creep.moveTo(target);
+    if(err != OK)
+        delete creep.memory.target;
 };
 
 module.exports = RoleBuilder;
